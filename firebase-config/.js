68635feba3e33779a8e49f2714e2b301f823abeb
ningmenghuang2017 <ningmeng.huang2017@gmail.com>
2026-08-01 19:21:24 -0700

    Create .js

---
+++ firebase-config/.js
@@ -0,0 +1,12 @@
+// Firebase web config for Ender chat app.
+// Fill/replace values from your Firebase Project Settings -> General -> Your apps (Web).
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDSUWEC2zJ1Ll6pozmlMk3Mtz776i5g1Fw",
+  authDomain: "webb-d532d.firebaseapp.com",
+  projectId: "webb-d532d",
+  storageBucket: "webb-d532d.firebasestorage.app",
+  messagingSenderId: "567828913754",
+  appId: "1:567828913754:web:d9885cc791e32a43c7c263",
+  measurementId: "G-K0E4TK3J10"
+};
